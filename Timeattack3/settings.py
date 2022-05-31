ad872608946804fd604e5061d289c25e303bf1ce
Roshu2 <kyumin1020@gmail.com>
2022-05-31 09:10:27 +0900

    1. 시크릿키 제거

--- Timeattack3/settings.py
+++ Timeattack3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0b6k8qcpr7eck@2+k%nqyyx3jm%76o-5z7x=vlo&o=_@%rx@(9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
